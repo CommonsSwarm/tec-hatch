71d9f38eb719ccd614a342e146a2866528d14dd2
David Llop <931684+sembrestels@users.noreply.github.com>
2020-11-25 17:29:22 +0100

    Delete unneded MainView

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,4 @@
-import { Order } from './order'
 import { Presale } from './presale'
 import { Polling } from './polling'
 
-export { Order, Presale, Polling }
+export { Presale, Polling }
--- src/context.js
+++ src/context.js
@@ -1,4 +1,3 @@
 import { createContext } from 'react'
 
-export const MainViewContext = createContext()
 export const PresaleViewContext = createContext()
