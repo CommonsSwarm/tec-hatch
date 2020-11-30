7d7d515bedcd15e3a5cb98136d39eae2bbec097a
PJColombo <paulo.colombo@pm.me>
2020-11-30 23:28:58 +0100

    Removing config data and actions from hook so we don't trigger unnecessary renders on those components that use them  when subscription handlers are called

--- src/hooks/useAppLogic.js
+++ src/hooks/useAppLogic.js
@@ -1,17 +1,9 @@
-import { useAppState } from '../providers/AppState'
-import useActions from './useActions'
 import { useContributionsSubscription } from './useSubscriptions'
 
 export const useAppLogic = () => {
-  const { config, errors, isLoading } = useAppState()
-  const actions = useActions(() => {}) // TODO: Pass closePanel function
   const contributions = useContributionsSubscription()
 
   return {
-    actions,
-    config,
     contributions,
-    isLoading: isLoading,
-    errors,
   }
 }
