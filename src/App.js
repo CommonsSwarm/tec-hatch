41598a017e537bb42ddf82e4eb91df78928dc998
PJColombo <paulo.colombo@pm.me>
2021-04-01 03:08:51 +0200

    HatchView component deleted. User token and contribtuion-related data refactored into its own context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import { Main, ToastHub } from '@tecommons/ui'
 import { WalletProvider } from './providers/Wallet'
+import { AppStateProvider } from './providers/AppState'
+import { UserStateProvider } from './providers/UserState'
 import { ConnectProvider as Connect } from './providers/Connect'
 
 import MainView from './components/MainView'
+import Hatch from './screens/Hatch'
 
 import './assets/global.css'
-import { AppStateProvider } from './providers/AppState'
-import HatchView from './views/HatchView'
 
 export default () => {
   return (
     <WalletProvider>
       <Connect>
         <AppStateProvider>
-          <Main theme="dark" assetsUrl="/aragon-ui" layout={false}>
-            <ToastHub>
-              <MainView>
-                <HatchView />
-              </MainView>
-            </ToastHub>
-          </Main>
+          <UserStateProvider>
+            <Main theme="dark" assetsUrl="/aragon-ui" layout={false}>
+              <ToastHub>
+                <MainView>
+                  <Hatch />
+                </MainView>
+              </ToastHub>
+            </Main>
+          </UserStateProvider>
         </AppStateProvider>
       </Connect>
     </WalletProvider>
