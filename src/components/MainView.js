ff8463ad9d1a85c39b4a1c285e01ea2d5ff9405f
PJColombo <paulo.colombo@pm.me>
2021-04-30 16:26:03 +0200

    TEC ui theme passed to styled components

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,33 +1,39 @@
 import React from 'react'
-import { ScrollView, SyncIndicator, Layout } from '@commonsswarm/ui'
+import { ScrollView, SyncIndicator, Layout, useTheme } from '@commonsswarm/ui'
+import { ThemeProvider as SCThemeProvider } from 'styled-components'
+
 import Header from './Header/Header'
+
 import { useAppState } from '../providers/AppState'
 
 const MainView = ({ children }) => {
+  const theme = useTheme()
   const { isLoading } = useAppState()
 
   return (
-    <ScrollView>
-      <div
-        css={`
-          display: flex;
-          flex-direction: column;
-          height: 100vh;
-        `}
-      >
+    <SCThemeProvider theme={theme}>
+      <ScrollView>
         <div
           css={`
-            flex-shrink: 0;
+            display: flex;
+            flex-direction: column;
+            height: 100vh;
           `}
         >
-          <Header />
+          <div
+            css={`
+              flex-shrink: 0;
+            `}
+          >
+            <Header />
+          </div>
+          <Layout>
+            <SyncIndicator visible={isLoading} />
+            <Layout>{!isLoading && children}</Layout>
+          </Layout>
         </div>
-        <Layout>
-          <SyncIndicator visible={isLoading} />
-          <Layout>{!isLoading && children}</Layout>
-        </Layout>
-      </div>
-    </ScrollView>
+      </ScrollView>
+    </SCThemeProvider>
   )
 }
 
